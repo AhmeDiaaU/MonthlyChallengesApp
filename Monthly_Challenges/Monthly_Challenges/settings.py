88b75f9d6b1b2b3c57e6c8edf70d2b4ba49d9b31
Unix <114300979+AhmeDiaaU@users.noreply.github.com>
2025-04-03 16:23:21 +0200

    add css file and improve look of the our website

--- Monthly_Challenges/Monthly_Challenges/settings.py
+++ Monthly_Challenges/Monthly_Challenges/settings.py
@@ -121,9 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/' # it tells django that 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+   BASE_DIR/"static"
+] # to make djange see static files outside our app 
